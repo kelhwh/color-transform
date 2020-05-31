895ff941699fb43b54dabb992ec8f1a743021fb1
Frank Hsieh <frank1571@gmail.com>
2020-05-31 19:59:53 +0100

    adding all variables selection in main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from module.transform import transform_my_pic
 artists_list = os.listdir('samples/')
 bits_list = [8, 16, 32]
 pic_list = os.listdir('test/')
+methods_list = ['closest', 'farthest']
 
 def pick_artist():
 
@@ -14,16 +15,22 @@ def pick_artist():
     #bits = input('How many bits you want the output to have?: ')
     bits_idx = SelectionMenu.get_selection([8,16,32], 'How many bits you want the output to have?')
 
+    artist = artists_list[artist_idx]
+    bits = bits_list[bits_idx]
 
-    return artists_list[artist_idx], bits_list[bits_idx]
-
+    return artist, bits
 
 
 def get_input_pic():
-    pic_list = os.listdir('test/')
-    target_pic = SelectionMenu.get_selection(pic_list, 'Which picture you want to transform?')
+    target_idx = SelectionMenu.get_selection(pic_list, 'Which picture you want to transform?')
+    target_pic = 'test/{}'.format(pic_list[target_idx])
 
     return target_pic
+
+def select_method():
+    method = SelectionMenu.get_selection(methods_list, 'Which picture you want to transform?')
+
+    return methods_list[method]
 
 
 def main():
@@ -31,12 +38,11 @@ def main():
     print ('Hello! Welcome to color transform!')
     artist, bits = pick_artist()
 
-
     image_list = cluster_paintings(artist, bits)
+    target_pic = get_input_pic()
+    method = select_method()
 
-    target_pic = 'test/{}'.format(pic_list[get_input_pic()])
-
-    transform_my_pic(artist, target_pic,bits,image_list, output_width=300)
+    transform_my_pic(artist, target_pic,bits,image_list, output_width=300, method=method)
 
 
 
